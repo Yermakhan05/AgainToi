bb8798989669ae891c7f66a747e3dd03fb9c8c09
yermakhankasymzhomart <k_yermakhan@kbtu.kz>
2024-10-23 02:46:38 +0500

    profile image

--- Django project/AgainToi/users/admin.py
+++ Django project/AgainToi/users/admin.py
@@ -4,8 +4,3 @@ from users.models import UserProfile
 
 
 # Register your models here.
-
-@admin.register(UserProfile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'User', 'Bio', 'ProfilePicture')
-    search_fields = ('Bio',)
